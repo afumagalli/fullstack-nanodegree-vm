2bcaf9f9c34aaa9d48f1ac4a99d7149b9d891061
Anthony Fumagalli <anthony.fumagalli@udacity.com>
2015-09-27 11:18:48 -0700

    Add code to create db and count players

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,24 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+DROP TABLE IF EXISTS matches, players;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE matches
+(
+matchID serial,
+winner int,
+loser int
+);
+
+CREATE TABLE players
+(
+playerID serial,
+name text,
+wins int,
+losses int
+);
